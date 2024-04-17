13aa71689cff48394298a1afce17dd450fa99d22
tddudwns1 <tjddudwns1@naver.com>
2024-04-18 00:32:51 +0900

    [BOJ] 9375. 패션왕 신해빈 👚

---
+++ 성영준/boj_9375_패션왕신해빈.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class boj_9375_패션왕신해빈 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int T = 1; T <= tc; T++) {
+            int n = Integer.parseInt(br.readLine());
+
+            Map<String, Integer> map = new HashMap();
+            for (int i = 0; i < n; i++) {
+
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                st.nextToken();
+                String key = st.nextToken();
+
+                map.put(key, map.getOrDefault(key, 0) + 1);
+            }
+
+            int total = 1;
+            for (int now : map.values()) {
+                total *= now + 1;
+            }
+
+            sb.append(total - 1).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
