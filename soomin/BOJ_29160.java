6867f5d56f567b64c8dd5d42466e1af1f525ca80
tnatna0801 <min120926@gmail.com>
2024-07-10 14:07:36 +0900

    [BOJ] 메모리 줄이기
    내부 클래스 제거

--- soomin/BOJ_29160.java
+++ soomin/BOJ_29160.java
@@ -8,20 +8,6 @@ import java.util.StringTokenizer;
 
 public class BOJ_29160 {
 
-    static class Player implements Comparable<Player>  {
-        int p;
-        int v;
-
-        Player(int p, int v) {
-            this.p = p;
-            this.v = v;
-        }
-
-        public int compareTo(Player o) {
-            return o.v - this.v;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -29,9 +15,11 @@ public class BOJ_29160 {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        Map<Integer, PriorityQueue<Player>> players = new HashMap<>();
+        Map<Integer, PriorityQueue<Integer>> players = new HashMap<>();
         for (int i = 1; i <= 11; i++) {
-            players.put(i, new PriorityQueue<>());
+            players.put(i, new PriorityQueue<>((o1, o2) -> {
+                return o2 - o1;
+            }));
         }
 
         for (int i = 0; i < N; i++) {
@@ -40,29 +28,27 @@ public class BOJ_29160 {
             int position = Integer.parseInt(st.nextToken());
             int value = Integer.parseInt(st.nextToken());
 
-            players.get(position).add(new Player(position, value));
+            players.get(position).add(value);
         }
 
         int[] team = new int[12];
         // 초기 팀 구성 -> 3월은 초기에만 중요하고 이후는 걍 11월 그래도 가니까 한번만 해도 될 듯
         for (int i = 1; i <= 11; i++) {
             if (players.get(i).isEmpty()) continue;
-            team[i] = players.get(i).poll().v;
+            team[i] = players.get(i).poll();
         }
 
         while (K-- > 0) {
 
             // 8월 가치 감소
             for(int i = 1; i<=11; i++) {
-                if(team[i] == 0) continue;
-                team[i]--;
-                players.get(i).add(new Player(i, team[i])); // 이럼 재정렬 흠 시간 괜찮나...
+                if(team[i] > 0) players.get(i).add(--team[i]);
             }
 
             // 11월 재선발
             for (int i = 1; i <= 11; i++) {
                 if (players.get(i).isEmpty()) continue;
-                team[i] = players.get(i).poll().v;
+                team[i] = players.get(i).poll();
             }
         }
 
