381b35e67178b6d1ae9630846436213e702abc9e
tnatna0801 <min120926@gmail.com>
2024-07-19 23:42:36 +0900

    [FIX]1141. 자료구조 변경
    그리디

--- soomin/Main.java
+++ soomin/Main.java
@@ -1,10 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.Set;
 
 public class Main {
     // 그리디? => 각 문자열을 이중 포문 N2, 문자열 비교시 M => O(N2M)
@@ -12,33 +11,33 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
-        PriorityQueue<String> sortedStrings = new PriorityQueue<>((o1, o2) -> {
-            return o2.length() - o1.length();
-        }); // 내림차순으로 정렬 => 부분집합이 제일 많아야하니까! 길이가 짧은 순으로 먼저 들어가버리면 접두사가 될 확률이 높다!
-        for(int i = 0; i<N; i++){
-            sortedStrings.add(br.readLine());
+        String[] str = new String[N];
+        for (int i = 0; i < N; i++) {
+            str[i] = br.readLine();
         }
 
-        Set<String> strings = new HashSet<>(); // 입력으로 중복값이 주어질 수 있음
-        strings.add(sortedStrings.poll()); // 하나 넣고 시작
-        
-        while(!sortedStrings.isEmpty()) {
+        Arrays.sort(str, Comparator.reverseOrder()); // 내림차순 => 그리디 탐색을 위하여 (긴 값이 먼저 채택되어야 가장 많은 부분집합을 선택할 수 있다. )
+        ArrayList<String> words = new ArrayList<>(); // => 굳이 자료구조를 만들지 않고 count만 셀 수는 없을까?
+        words.add(str[0]);
 
-            String target = sortedStrings.poll();
+
+        for (int i = 1; i < N; i++) {
+
+            String target = str[i];
             boolean isPrefix = false;
 
-            for(String str : strings) {
-                // 최적화: target이 str보다 작거나 같을때만 비교함 (target이 더 크면 접두사가 될 수 없음)
-                if(target.length() > str.length()) continue;
-                if(str.indexOf(target) == 0) {// 0번째 인덱스에 target이 있으면 접두사임
+            for(String word : words) {
+                if(target.length() > word.length()) continue;   // 최적화: target이 str보다 작거나 같을때만 비교함 (target이 더 크면 접두사가 될 수 없음)
+                if(word.indexOf(target) == 0) { // 0번째 인덱스에 target이 있으면 접두사임
                     isPrefix = true;
                     break;
                 }
             }
-            if(!isPrefix) strings.add(target); // 삽입
+
+            if(!isPrefix) words.add(target);
         }
 
-        System.out.println(strings.size());
+        System.out.println(words.size());
+
     }
 }
