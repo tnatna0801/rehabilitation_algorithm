283f6377d1da56766e589b08d59b504637d9df96
tnatna0801 <min120926@gmail.com>
2024-07-20 00:21:41 +0900

    [FIX]1141. 로직 변경
    그리디

--- soomin/Main.java
+++ soomin/Main.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class Main {
     // 그리디? => 각 문자열을 이중 포문 N2, 문자열 비교시 M => O(N2M)
@@ -16,28 +14,15 @@ public class Main {
             str[i] = br.readLine();
         }
 
-        Arrays.sort(str, Comparator.reverseOrder()); // 내림차순 => 그리디 탐색을 위하여 (긴 값이 먼저 채택되어야 가장 많은 부분집합을 선택할 수 있다. )
-        ArrayList<String> words = new ArrayList<>(); // => 굳이 자료구조를 만들지 않고 count만 셀 수는 없을까?
-        words.add(str[0]);
+        Arrays.sort(str);
 
-
-        for (int i = 1; i < N; i++) {
-
-            String target = str[i];
-            boolean isPrefix = false;
-
-            for(String word : words) {
-                if(target.length() > word.length()) continue;   // 최적화: target이 str보다 작거나 같을때만 비교함 (target이 더 크면 접두사가 될 수 없음)
-                if(word.indexOf(target) == 0) { // 0번째 인덱스에 target이 있으면 접두사임
-                    isPrefix = true;
-                    break;
-                }
-            }
-
-            if(!isPrefix) words.add(target);
+        int count = 1;
+        for (int i = 1; i < N; i++) { // 버블정렬? => 인접한 원소만 비교해도 된다. 왜냐면 알파벳 순으로 정렬했으니까 그게 보장이 된다!
+            if (str[i].equals(str[i-1])) continue;
+            if (str[i].indexOf(str[i - 1]) == 0) continue;
+            count++;
         }
 
-        System.out.println(words.size());
-
+        System.out.println(count);
     }
 }
