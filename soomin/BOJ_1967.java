eebe0427f411278c3f1425a752dcf480829ac912
tnatna0801 <min120926@gmail.com>
2024-08-02 15:24:31 +0900

    [BOJ]1967. 시간 개선

--- soomin/BOJ_1967.java
+++ soomin/BOJ_1967.java
@@ -16,8 +16,8 @@ public class BOJ_1967 {
 
     static int max = 0;
     static List<Node>[] nodes;
-    static Map<Integer, Map<Integer, Integer>> nodeMap;
     static boolean[] visited;
+    static int maxIndex = 1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -37,32 +37,37 @@ public class BOJ_1967 {
             int w = Integer.parseInt(st.nextToken());
 
             nodes[s].add(new Node(e, w));
-            nodes[e].add(new Node(s, w));
+            nodes[e].add(new Node(s, w)); // 리프노드를 기준으로 탐색을 역으로 해야하기 때문
         }
 
         // dfs로 깊이우선탐색을하면서 가장 긴 구간을...갱신?
         // 부모 어디서 부터 어디까지 더하기를 해야하는 걸까? => 모든 노드에서 깊이우선 탐색 갈기기? 그중 젤 긴거 두개?
         // 어떤 두 노드를 선택해도 둘 사이에 경로가 항상 1개만 존재한다는 말은 무조건 이진트리라는 걸까
-        // 모든 노드는 불필요해보인다 어쨋든 리프노드까지 탐색을 할때니 리프노드들만 돌면 되지 않을까?
-        for (int i = 1; i <= n; i++) {
-            if(nodes[i].size() == 1) {
-                visited = new boolean[n+1];
-                visited[i] = true;
-                dfs(i, 0);
-            }
-        }
+
+        // 루트를 기준으로 가장 비용이 큰 리프노드를 찾!
+        visited = new boolean[n + 1];
+        visited[1] = true;
+        dfs(1, 0);
+
+        // 찾은 리프노드를 기준으로 경로를 계산
+        visited = new boolean[n + 1];
+        visited[maxIndex] = true;
+        dfs(maxIndex, 0);
 
         System.out.println(max);
     }
 
     /**
-     * dfs로 리프노드들을 탐색하면서 가장 최대 비용을 가진 경로를 찾습니다.
+     * dfs로 노드를 탐색하면서 가장 최대 비용을 가진 경로를 찾습니다.
      * @param index 리프노드의 번호
      * @param sum 비용 합산
      */
     private static void dfs(int index, int sum) {
 
-        max = Math.max(sum, max);
+        if(max < sum) {
+            max = sum;
+            maxIndex = index;
+        }
 
         for(Node next : nodes[index]) {
             if(visited[next.end]) continue; // 방문체크!
