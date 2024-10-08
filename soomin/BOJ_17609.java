4bcc5e61445e4a83498273a8e8566950fdc99b8f
tnatna0801 <min120926@gmail.com>
2024-10-08 19:57:23 +0900

    [BOJ]17609. 회문
    투 포인터

--- soomin/BOJ_17609.java
+++ soomin/BOJ_17609.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ_17609 {
+
+    static int start, end;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,40 +13,42 @@ public class BOJ_17609 {
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        while(T-- > 0) {
+        while (T-- > 0) {
 
             String word = br.readLine();
 
-            int start = 0, end = word.length()-1;
-            int diffCnt = 0;
+            start = 0;
+            end = word.length() - 1;
 
-            sb.append(palindrome(word, start, end, diffCnt));
-
-            sb.append("\n");
+            sb.append(palindrome(word)).append("\n");
         }
 
         System.out.print(sb);
 
     }
 
-    private static int palindrome(String word, int start, int end, int diffCnt) {
-        while(start < end) {
+    private static int palindrome(String word) {
 
-            if(word.charAt(start) == word.charAt(end)) {
-                start++;
-                end--;
-            }
-            else if(diffCnt == 1) {
-                return 2;
-            }
-            else {
+        if (isEqual(word)) return 0;
 
-                return Math.min(palindrome(word, start+1, end, diffCnt+1),
-                        palindrome(word, start, end-1, diffCnt+1));
+        int tmpS = start++;
+        int tmpE = end;
+        if (isEqual(word)) return 1;
 
-            }
+        start = tmpS;
+        end = --tmpE;
+        if (isEqual(word)) return 1;
+
+        return 2;
+    }
+
+    private static boolean isEqual(String word) {
+        while (start < end) {
+            if (word.charAt(start) != word.charAt(end)) return false;
+            start++;
+            end--;
         }
 
-        return diffCnt;
+        return true;
     }
 }
